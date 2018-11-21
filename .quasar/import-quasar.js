e57d26b4ccb0a3c39a225a773ab14547cf72d53d
Tamara Ayres - Landix <tamara@landix.com.br>
2018-11-21 13:18:56 -0200

    Treinamento Web (VueJs e Quasar)

---
+++ .quasar/import-quasar.js
@@ -0,0 +1,19 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding initialization code.
+ * Use "quasar new plugin <name>" and add it there.
+ * One plugin per concern. Then reference the file(s) in quasar.conf.js > plugins:
+ * plugins: ['file', ...] // do not add ".js" extension to it.
+ **/
+
+import lang from 'quasar-framework/i18n/pt-br'
+
+
+import Vue from 'vue'
+
+import {Quasar,QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QField,QInput,QCard,QCardTitle,QCardMain,QCardSeparator,Ripple,Notify,LocalStorage} from 'quasar'
+
+
+Vue.use(Quasar, { config: {},i18n: lang,components: {QLayout,QLayoutHeader,QLayoutDrawer,QPageContainer,QPage,QToolbar,QToolbarTitle,QBtn,QIcon,QList,QListHeader,QItem,QItemMain,QItemSide,QField,QInput,QCard,QCardTitle,QCardMain,QCardSeparator},directives: {Ripple},plugins: {Notify,LocalStorage} })
