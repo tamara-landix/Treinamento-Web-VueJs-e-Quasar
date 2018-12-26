a8031bc3b2643fe3a2a65db0f7a031d9a60034b8
Tamara Ayres - Landix <tamara@landix.com.br>
2018-12-26 17:35:13 -0200

    Exercício 4

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -10,6 +10,13 @@
       :loading="loading"
       @request="request"
     />
+      <template slot="top-left" slot-scope="props">
+        <q-search hide-underline float-label="Nome" placeholder=" " v-model="filterName" @input="filterTable" />
+        <q-search hide-underline float-label="LDAP UID" placeholder=" " v-model="filterLdapUid" @input="filterTable" />
+        <q-radio v-model="filterActive" val="" label="Todos" @input="filterTable" />
+        <q-radio v-model="filterActive" val="1" label="Ativos" @input="filterTable" />
+        <q-radio v-model="filterActive" val="0" label="Inativos" @input="filterTable" />
+      </template>
       <q-tr slot="body" slot-scope="props" :props="props">
         <q-td key="name" :props="props">{{ props.row.name }}</q-td>
         <q-td key="email" :props="props">{{ props.row.email }}</q-td>
@@ -32,32 +39,99 @@
 
 <script>
 export default {
+  name: 'Users',
   data: () => ({
+    loading: false,
+    filter: {},
+    serverPagination: {
+      page: 1,
+      rowNumber: undefined
+    },
+    serverData: [],
     columns: [
       {
-        name: 'desc',
+        name: 'name',
         required: true,
-        label: 'Dessert (100g serving)',
+        label: 'Nome',
         align: 'left',
         field: 'name',
-        sortable: true,
-        classes: 'my-class',
-        style: 'width: 500px'
-      }
-    ],
-    tableData: [
+        sortable: true
+      },
       {
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 4.0,
-        sodium: 87,
-        calcium: '14%',
-        iron: '1%'
+        name: 'email',
+        required: true,
+        label: 'E-mail',
+        align: 'left',
+        field: 'email',
+        sortable: true
+      },
+      {
+        name: 'ldap_uid',
+        required: true,
+        label: 'LDAP UID',
+        align: 'left',
+        field: 'ldap_uid',
+        sortable: true
+      },
+      {
+        name: 'created',
+        required: true,
+        label: 'Data de criação',
+        align: 'left',
+        field: 'created',
+        sortable: true
+      },
+      {
+        name: 'active',
+        required: true,
+        label: 'Ativo',
+        align: 'left',
+        field: 'active',
+        sortable: true
       }
     ]
-  })
+  }),
+  mounted () {
+    this.request({
+      pagination: this.serverPagination,
+      filter: this.filter
+    })
+  },
+  methods: {
+    request ({ pagination, filter }) {
+      this.loading = true
+
+      console.log(pagination)
+      this.$axios
+        .get(`/landix/user/`, { params: {
+          limit: pagination.rowsPerPage,
+          offset: (pagination.page - 1) * pagination.rowsPerPage,
+          name__icontains: filter,
+          ordering: pagination.sortBy ? (pagination.descending ? '-' : '') + pagination.sortBy : undefined
+        } })
+        .then(({ data }) => {
+          this.serverPagination = pagination
+          this.serverPagination.rowNumber = data.count
+          this.serverData = data.results
+          this.loading = false
+        })
+        .catch(error => {
+          console.error(error)
+          this.loading = false
+        })
+    },
+    filterTable () {
+      this.filter = {
+        name: this.filterName,
+        ldapUid: this.filterLdapUid,
+        active: this.filterActive
+      }
+      this.request({
+        pagination: this.serverPagination,
+        filter: this.filter
+      })
+    }
+  }
 }
 </script>
 
